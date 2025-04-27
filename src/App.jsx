e259c8a71137022608d8007762eb7c95c0f4acb7
yancarlos4500 <jiancarlosparedes@mybihs.org>
2025-04-26 22:41:29 -0400

    update server url

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { io } from 'socket.io-client';
 import FlightMap from './FlightMap';
 import './index.css';
 
-// const socket = io('https://zsu-coordination-server-production.up.railway.app/');
-const socket = io('http://localhost:3001');
+const socket = io('https://zsu-coordination-server-production.up.railway.app/');
+// const socket = io('http://localhost:3001');
 const COLUMNS = ['Callsign', 'Waypoint', 'Center Estimate', 'Pilot Estimate', 'Altitude', 'Mach', 'Status'];
 
 function Table({ title, data, toggleColor, updateField }) {
